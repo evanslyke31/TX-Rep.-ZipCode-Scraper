891c63c8b9bc579edda090a91477dffc497ca40d
evanslyke31 <erikvslyke@gmail.com>
2021-04-03 21:06:20 -0500

    initizialized project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TX_Rep._ZipCode_Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
