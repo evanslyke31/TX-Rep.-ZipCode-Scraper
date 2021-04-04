79a8159ab0199b2776a8fd5f7eb54511831ff57c
Erik Van Slyke <evanslyke31@gmail.com>
2021-04-03 23:37:23 -0500

    Added PDF scrapping

--- Program.cs
+++ Program.cs
@@ -3,6 +3,12 @@ using HtmlAgilityPack;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using iText.Kernel.Pdf;
+using System.Text;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace TX_Rep._ZipCode_Scraper
 {
@@ -30,19 +36,58 @@ namespace TX_Rep._ZipCode_Scraper
 
             htmlDoc = web.Load(homePageUrl);
 
-            var pdfLink = htmlDoc.DocumentNode.SelectSingleNode("//a['ZIP Codes by District']").Attributes["href"].Value;
+            var pdfLink = htmlDoc.DocumentNode.SelectSingleNode("//a[.='ZIP Codes by District']").Attributes["href"].Value;
 
-            var pdfStream = new MemoryStream();
-            var reader = PDFReader(pdfStream);
-            ExtractPDFText(pdf);
+            using (MemoryStream pdfStream = new MemoryStream())
+            {
+                ConvertToStream(pdfLink, pdfStream);
+                pdfStream.Seek(0, SeekOrigin.Begin);
+
+                using var reader = new PdfReader(pdfStream);
+                using var pdfDoc = new PdfDocument(reader);
+                ExtractPDFText(pdfDoc);
+
+            }
 
             Console.WriteLine(pdfLink);
         }
 
-        private static List<string> ExtractPDFText(PDFDocument pdf) {
+        private static void ConvertToStream(string fileUrl, Stream stream)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(fileUrl);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
+            try
+            {
+                Stream response_stream = response.GetResponseStream();
 
-            return new List<string>();
+                response_stream.CopyTo(stream, 4096);
+            }
+            finally
+            {
+                response.Close();
+            }
+        }
+
+        private static List<string> ExtractPDFText(PdfDocument pdf) {
+            var zipCodes = new List<string>();
+            StringBuilder processed = new StringBuilder();
+            var strategy = new LocationTextExtractionStrategy();
+            for (int i = 1; i<=pdf.GetNumberOfPages(); ++i)
+			{
+                var page = pdf.GetPage(i);
+                string text = PdfTextExtractor.GetTextFromPage(page, strategy);
+                Console.Write(text);
+                processed.Append(text);
+			}
+            string pattern = @"\*[0-9]{5}";
+            Regex rgx = new Regex(pattern);
+            foreach (Match match in rgx.Matches(processed.ToString()))
+            {
+                zipCodes.Add(match.Value.Replace("*", ""));
+                Console.WriteLine(match.Value);
+            }
+            return zipCodes;
         }
 
         private static List<string> GetMembersInfoLinks(string url) {
